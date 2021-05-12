b0707a1f00fedabc45423a0780445ca113df131e
Vanessa <vantaidesign8@gmail.com>
2021-05-12 14:27:30 +0100

    Made few changes to my JavaScript code.

--- js/script.js
+++ js/script.js
@@ -88,29 +88,25 @@ $(document).ready(function() {
 		}
 	});
 
-$("#filters a").click(function(e) {
-	e.preventDefault()
+	$("#filters a").click(function(e) {
+		e.preventDefault()
 
-	$("#filters .current").removeClass("current");
-	$(this).addClass("current");
+		$("#filters .current").removeClass("current");
+		$(this).addClass("current");
 
-	let selector = $(this).after("data-filter");
+		let selector = $(this).after("data-filter");
 
-	$(".items").isotope({
-		filter: selector,
-		animationOptions: {
-			duration: 1500,
-			easing: 'linear',
-			queue: false
-		}
-	});
+		$(".items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 1500,
+				easing: 'linear',
+				queue: false
+			}
+		});
 
-	return false;
-}); 
-
-
-
-
+		return false;
+	}); 
 });
 
 
