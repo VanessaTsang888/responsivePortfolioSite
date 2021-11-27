c94e52c2503036f9438929d14e7eaa0e5e4b3ca8
Vanessa <vantaidesign8@gmail.com>
2021-11-27 22:38:04 +0000

    I have updated the content for header slider, intro section, tech skills section and portfolio navigation.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Software Engineer.", "Web Developer.", "UX Designer."],
+		strings: ["Software Engineer.", "Frontend Developer.", "Digital Accessibility."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
